47679c1f4ecc0b9a75fa5fd6d698aa811011897f
hieronymusma <maurice_hieronymus@yahoo.de>
2019-01-14 00:05:56 +0100

    Initial commit -> working connection between client and server

---
+++ Server/Server/BetHub/BetHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server.BetHub
+{
+    public class BetHub : Hub
+    {
+        public string TestMe(string message)
+        {
+            return message;
+        }
+
+        public void ServerCall()
+        {
+            Clients.All.SendAsync("ClientCall");
+        }
+    }
+}
---
+++ Server/Server/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Server
+{
+    public class Startup
+    {
+        private const int ANGULAR_PORT = 4200;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .WithOrigins($"http://localhost:{ANGULAR_PORT}", $"http://{Environment.MachineName}:{ANGULAR_PORT}", $"http://{GetLocalIpAddress()}:{ANGULAR_PORT}");
+            }));
+
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseCors("CorsPolicy");
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<BetHub.BetHub>("/BetHub");
+            });
+        }
+
+        private string GetLocalIpAddress()
+        {
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            return localIP;
+        }
+    }
+}
