4e91a74249df12d68a9a4176cf5d6d84fbfec82c
hieronymusma <Tischmond81>
2019-02-17 17:28:39 +0100

    Getting things running

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { RouterModule, Routes } from "@angular/router";
+
 import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { BetComponent } from "./bet/bet.component";
+
+const appRoutes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/bet" },
+  { path: "bet", component: BetComponent },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
