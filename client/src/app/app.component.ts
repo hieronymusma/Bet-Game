26cbf485e854b1843759523e414132716671364e
hieronymusma <Tischmond81>
2019-02-17 17:05:44 +0100

    Clean up

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { DataService } from "./data.service";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent implements OnInit {
-  title = "client";
-
-  constructor(private dataService: DataService) { }
-
-  ngOnInit(): void {
-    this.Test();
-  }
-
-  private async Test() {
-    await this.dataService.connect();
-    this.dataService.TestMe().then(() => console.log("TestMe successful"));
-    this.dataService.ServerCall();
-  }
-}
+export class AppComponent { }
