fe763b780c1db6c7b51d6c0ed6fbb098885cf040
hieronymusma <maurice_hieronymus@yahoo.de>
2019-03-01 18:38:23 +0100

    Remove changing of background color

--- client/src/app/components/wait/wait.component.ts
+++ client/src/app/components/wait/wait.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ElementRef } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgxSpinnerService } from "ngx-spinner";
 import { DataService } from "src/app/services/data.service";
 import { Router } from "@angular/router";
@@ -10,7 +10,7 @@ import { BetTarget } from "src/app/server-interfaces/transaction";
   templateUrl: "./wait.component.html",
   styleUrls: ["./wait.component.scss"]
 })
-export class WaitComponent implements OnInit, AfterViewInit {
+export class WaitComponent implements OnInit {
 
   private get betTarget(): BetTarget {
     return this.selectedBetTarget.selectedTarget;
@@ -20,17 +20,11 @@ export class WaitComponent implements OnInit, AfterViewInit {
     private spinnerService: NgxSpinnerService,
     private dataService: DataService,
     private router: Router,
-    private selectedBetTarget: SelectedBetTargetService,
-    private elementRef: ElementRef) { }
+    private selectedBetTarget: SelectedBetTargetService) { }
 
   ngOnInit() {
     this.spinnerService.show();
     this.dataService.waitingFinished$.subscribe(() => this.redirectToBetComponent());
-  }
-
-  ngAfterViewInit(): void {
-    const color = this.betTarget === BetTarget.Blue ? "rgba(0,123,255,0.5)" : "rgba(220,53,69,0.5)";
-    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = color;
   }
 
   private redirectToBetComponent() {
